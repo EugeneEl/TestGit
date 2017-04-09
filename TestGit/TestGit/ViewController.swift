25971cee41df6df2736f679c55aeb60694481578
Eugene Goloboyar <eugene.goloboyar@yandex.ru>
2017-04-09 16:55:25 +0300

    testgit

---
+++ TestGit/TestGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGit
+//
+//  Created by Eugene Goloboyar on 09.04.17.
+//  Copyright © 2017 ELM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
